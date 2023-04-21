c73e799c4ee878af6affcf6073a7e91c75ba9de0
aprise11 <agnetasilta@gmail.com>
2023-04-21 09:50:18 +0300

    Added the GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -2,5 +2,27 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by{" "}
+        <a
+          href="https://prismatic-panda-2ddff3.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Agneta Prise
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/aprise11/react-weather-project"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
